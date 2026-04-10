7474d57428418bdd2dd8759263e2adde8062db12
Ganteda Abhishek <ganteda_abhishek@epam.com>
2026-04-10 16:33:12 +0530

    implemented products and categories sections for frontend

--- frontend/organic-store-frontend/src/app/features/home/home.ts
+++ frontend/organic-store-frontend/src/app/features/home/home.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { Hero } from './components/hero/hero';
 import { Features } from './components/features/features';
+import { Products } from './components/products/products';
+import { Category } from './components/category/category';
+
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, Features],
+  imports: [Hero, Features, Products, Category],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
